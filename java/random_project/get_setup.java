9c74d0e53552b85299fa56929c2fe4685b57da19
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-10-16 23:10:21 -0500

    Create get_setup.java

---
+++ java/random_project/get_setup.java
@@ -0,0 +1,7 @@
+package get_setup.get_setup.get_setup;
+
+public interface get_setup {
+  public String net_type(String type);
+  public String use_as(String use_net_as);
+  public boolean is_ready();
+}
