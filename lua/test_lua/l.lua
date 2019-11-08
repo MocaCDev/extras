f5976b845e1116c1ab076ebdb1cd65caf2bd8160
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-11-07 21:51:07 -0600

    Update l.lua

--- lua/test_lua/l.lua
+++ lua/test_lua/l.lua
@@ -1,5 +1,11 @@
 require 'get_name'
 require 'get_number'
+require 'check_ip'
 
+-- Should just print the IP address
+-- If the IP address was not 127.0.0.1, it would print a different message
+print(check_ip("127.0.0.1"))
+-- Should print AIDAN
 print(get_name("AIDAN"))
+-- Should print the number 500 + 200 * 2
 print(get_number(500,200))
