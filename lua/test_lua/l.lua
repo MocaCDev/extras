9191fcad2d787fae5314a18c2e3903a071b668d4
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-11-07 21:41:43 -0600

    Update l.lua

--- lua/test_lua/l.lua
+++ lua/test_lua/l.lua
@@ -2,4 +2,10 @@ function get_name(name)
   return name
 end
 
+function get_number(num1,num2)
+  local number = num1+num2*4
+  return math.sqrt(number)
+end
+
+print(get_number(500,200))
 print(get_name("aidan"))
