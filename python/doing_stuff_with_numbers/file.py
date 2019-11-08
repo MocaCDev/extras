007a31484141ad32e24900549d43348295aff0c4
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-11-07 20:34:47 -0600

    Create file.py

---
+++ python/doing_stuff_with_numbers/file.py
@@ -0,0 +1,37 @@
+from time import sleep
+from math import floor
+import json
+d = []
+r = True
+while r:
+  get_num_one = int(input('Number One >> '))
+  get_num_two = int(input('Number Two >> '))
+  total = get_num_one * get_num_two
+  for i in range(total):
+    d.append(i)
+    print(i+1)
+  r =False
+op = open('data.txt','w')
+op.write(str(d))
+op.close()
+
+FAILED_ATTEMPTS = []
+SUCCESS_ATTEMPTS = []
+for i in d:
+  try:
+    get_mod = int(input('Remainder of number divided by >> '))
+    if d[i] % get_mod == 0:
+      FAILED_ATTEMPTS.append({f'failed_attempts':{i:i/8,'returned':0}})
+      print('Failed on # ' + str(i) + '\nReturned ' + str(floor(i / 8)))
+      sleep(.000000001)
+    else:
+      SUCCESS_ATTEMPTS.append({f'success_attempts':{i:f"{d[i]}*{d[i+1]}",'returned':d[i]*d[i+1]}})
+      FAILED_ATTEMPTS[0]['failed_attempts'].update(SUCCESS_ATTEMPTS[0]['success_attempts'])
+      print(str(d[i] * d[i+1] + 10000) + '\n')
+  except IndexError:
+    print('Done')
+
+with open('data.json','w') as file:
+  to_json = json.dumps(FAILED_ATTEMPTS,indent=2,sort_keys=False)
+  file.write(to_json)
+  file.close()
