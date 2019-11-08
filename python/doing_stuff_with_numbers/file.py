c6b3246606cbe35874832d011f5954385bbb0a83
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-11-07 20:39:14 -0600

    Update file.py

--- python/doing_stuff_with_numbers/file.py
+++ python/doing_stuff_with_numbers/file.py
@@ -17,16 +17,16 @@ op.close()
 
 FAILED_ATTEMPTS = []
 SUCCESS_ATTEMPTS = []
+get_mod = int(input('Remainder of number divided by >> '))
 for i in d:
   try:
-    get_mod = int(input('Remainder of number divided by >> '))
     if d[i] % get_mod == 0:
       FAILED_ATTEMPTS.append({f'failed_attempts':{i:i/8,'returned':0}})
       print('Failed on # ' + str(i) + '\nReturned ' + str(floor(i / 8)))
       sleep(.000000001)
     else:
       SUCCESS_ATTEMPTS.append({f'success_attempts':{i:f"{d[i]}*{d[i+1]}",'returned':d[i]*d[i+1]}})
-      FAILED_ATTEMPTS[0]['failed_attempts'].update(SUCCESS_ATTEMPTS[0]['success_attempts'])
+      FAILED_ATTEMPTS[0].update({'SUCCESS_ATTEMPTS':SUCCESS_ATTEMPTS})
       print(str(d[i] * d[i+1] + 10000) + '\n')
   except IndexError:
     print('Done')
