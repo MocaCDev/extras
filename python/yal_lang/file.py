5c2c0fede00f2838f908257a8c52e5cfd928e2de
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-11-16 20:55:31 -0600

    Update file.py

--- python/yal_lang/file.py
+++ python/yal_lang/file.py
@@ -1,9 +1,11 @@
 import os
 from lang import read_
 
+MAIN_PATH = os.environ.get('HOME')
+
 get_file = input('File: ')
 
-get_file = os.path.abspath(get_file)
+get_file = MAIN_PATH + '/' + get_file
 
 if not '.yal' in get_file:
   raise Exception('Cannot open and execute ' + get_file + '\nNot a .yal file')
