a004d330f46d8533eb75997c9036ef3c03de7ce0
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-12-10 13:19:00 -0600

    Update file.py

--- python/yal_lang/file.py
+++ python/yal_lang/file.py
@@ -7,7 +7,11 @@ get_file = []
 
 for i in range(len(os.listdir())):
   if '.yal' in os.listdir()[i]:
+    if 'main.yal' == os.listdir()[i]:
+      os.remove
+      raise NameError('The name ' + os.listdir()[i] + ' cannot be used')
     get_file.append(main_path + '/' + os.listdir()[i])
+
 
 #if not '.yal' in get_file:
   #raise Exception('Cannot open and execute ' + get_file + '\nNot a .yal file')
