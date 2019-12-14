af35c0ba35eb07cc15d6532f04d5e890e152262a
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-12-13 22:58:23 -0600

    Update file.py

--- python/yal_lang/file.py
+++ python/yal_lang/file.py
@@ -7,9 +7,9 @@ get_file = []
 
 for i in range(len(os.listdir())):
   if '.yal' in os.listdir()[i]:
-    if os.listdir()[i] == 'file.yal' or 'main.yal':
+    if os.listdir()[i] == 'file.yal':
       raise NameError('The name ' + os.listdir()[i] + ' cannot be used\n\nThe file ' + os.listdir()[i] + ' has been deleted')
-      os.remove('main.yal')
+      os.remove('file.yal')
     get_file.append(main_path + '/' + os.listdir()[i])
 
 
