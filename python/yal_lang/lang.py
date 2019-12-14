58e70fd4eb3a558854c8ee89ea59c78c55903b8f
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-12-13 23:36:46 -0600

    Update lang.py

--- python/yal_lang/lang.py
+++ python/yal_lang/lang.py
@@ -33,8 +33,8 @@ def read_(**file_to_read):
     open_file = open_file.replace('trying','try')
     open_file = open_file.replace('except err of','except')
     open_file = open_file.replace('con','f')
-    open_file = open_file.replace('!dict','**')
-    open_file = open_file.replace('!tup','*')
+    open_file = open_file.replace('!dict-','**')
+    open_file = open_file.replace('!tup-','*')
 
     open_file = open_file.replace('keep_track',open(f'{os.environ.get("HOME")}/extras/python/yal_lang/templates/keep_track_of_import.txt','r').read())
     open_file = open_file.replace('print_value','print')
