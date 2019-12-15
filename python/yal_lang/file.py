fb08b81ca417423557455d703cd6286b2cfbb7cd
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-12-15 09:36:57 -0600

    Update file.py

--- python/yal_lang/file.py
+++ python/yal_lang/file.py
@@ -2,8 +2,8 @@ import os
 from lang import read_
 
 main_path = os.environ.get('HOME')
-path = os.getcwd().replace('/',' ')
-print(path)
+path = os.getcwd().replace('/extras/python/yal_lang',' ')
+os.system(f'cd {path}')
 get_file = []
 
 for i in range(len(os.listdir())):
