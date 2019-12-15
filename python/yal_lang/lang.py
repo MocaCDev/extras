eaf3fc98f72d170bcef67dfc0d757ef0296f4d14
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-12-15 10:19:24 -0600

    Update lang.py

--- python/yal_lang/lang.py
+++ python/yal_lang/lang.py
@@ -87,5 +87,4 @@ def read_(**file_to_read):
 
     os.system('clear')
     print(Fore.WHITE + 'EXECUTING_MAIN -> ' + file_to_read['files'][i])
-    if i == len(file_to_read['files'])-1:
-      exec(open(file_to_read['files'][i].replace('.yal','.py'),'r').read())
+    exec(open(file_to_read['files'][i].replace('.yal','.py'),'r').read())
