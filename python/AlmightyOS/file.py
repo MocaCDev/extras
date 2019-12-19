e0ff0ad904748a8ef9327a18cd0bcd882ddf3c1c
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-12-18 22:20:09 -0600

    Update file.py

--- python/AlmightyOS/file.py
+++ python/AlmightyOS/file.py
@@ -1,8 +1,7 @@
 from files.server_setup import server_side
-from __init__ import _composing_
-
-_composing_(server_side.__composing__('.'))
 
 server = server_side()
 server._server_side_()
 server.__up_status__()
+server._compose_()
+server.__power_up__()
