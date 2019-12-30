8ae6a68c236f4fdc073f1985a5da09e042595f9c
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-12-30 13:52:58 -0600

    Create something_else.py

---
+++ python/something_new!/something_else.py
@@ -0,0 +1,17 @@
+from typing import Tuple, Generic, TypeVar, List, Callable
+
+T = TypeVar('T')
+
+class server(Generic[T]):
+
+  def _gather_(server:List[str],call_render:Callable[[str,bool],None],append_server_info:List[str]=[]) -> Tuple[list,list,int]:
+
+    for i in range(len(server)):
+      append_server_info.append(server[i])
+    
+    if len(append_server_info) > 3:
+      raise MemoryError('Cannot store more than 3 servers')
+
+    return server,call_render,0
+
+print(server._gather_(['180'],['180',True]))
