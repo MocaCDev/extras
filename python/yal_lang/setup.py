822961423f9a0f0eea5ecd5e0d2672ac9bc78baf
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-12-31 20:19:20 -0600

    Create setup.py

---
+++ python/yal_lang/setup.py
@@ -0,0 +1,23 @@
+setup(
+    name = "Yal Lang",
+    version = '1.0.2.1',
+    url = "https://github.com/ARACADERISE/extras",
+    license = "MIT License",
+    author = "ARACADERISE",
+    description = "Lightweight version of Python, easily compiled, easily written.",
+    long_description = "Secondary version of Python. Uses same compiler as Python, same syntax code as Python, just different keywords, key args, and compiled separetly than Python files",
+    packages = [],
+    include_package_data = True,
+    platforms = "LINUX",
+    install_requires = requirements.txt,
+    classifiers = [
+        "Development Status :: 1 - Beta",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Programming Language/Coding",
+        "Topic :: Development :: Application development",
+        "Topic :: Development"
+    ]
+)
