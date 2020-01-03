930e593b845d866184fd97ac9327ad0634f5c89b
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-01-03 02:05:26 -0600

    Update to_export.js

--- javascript/importing_exporting/to_export.js
+++ javascript/importing_exporting/to_export.js
@@ -1,5 +1,19 @@
+
+// ONE WAY
 function gather(name,age) {
   return (name,age);
 }
 
 export {gather};
+
+// HERE IS ANOTHER WAY
+
+export function hey(msg) {
+  alert(msg);
+}
+
+// HERE IS ANOTHER WAY
+
+export default function say_bye(bye_msg) {
+  alert(bye_msg);
+}
