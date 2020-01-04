b338ee49323ed5af4406ea5e2a99d253000c0b7a
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-01-03 18:06:01 -0600

    Update errors.py

--- python/yal_lang/client_plugin/errors.py
+++ python/yal_lang/client_plugin/errors.py
@@ -7,3 +7,16 @@ class IsAPrivateFile(Exception):
     NOTE:
       There will be a Yal Application compiler that compiles the locked file
   """
+
+class SlugTimeError(TimeoutError):
+  "this will be raised if the user sets a _slug_read_ timer for over 150 seconds"
+
+  """
+    The _slug_read_ function basically opens a file, and returns the result at a default time rate of 25 seconds.
+
+    The user can change the default time rate to whatever they want, but must be under 150.
+    
+    NOTE:
+      The soul purpose of the _slug_read_ function is to compile the file completely if it is greater than
+      200 bytes, using os.stat().st_size, which just gets the total size(bytes) of a file.
+  """
