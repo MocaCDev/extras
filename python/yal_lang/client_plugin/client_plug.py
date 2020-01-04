bd0ec120070f3e27ba952f0d39486c38f8df31e6
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-01-04 15:51:43 -0600

    Update client_plug.py

--- python/yal_lang/client_plugin/client_plug.py
+++ python/yal_lang/client_plugin/client_plug.py
@@ -1,3 +1,55 @@
 # this will be a plugin for the Yal Language App client
+# and the Platform of which the user assigns the Yal
+# app to
 
-class yal_client_plugin:pass
+import os,json,sys
+
+class yal_client_plugin:
+
+  def _complete_platform_(self,storage_amount:'int'=500000):
+
+    """this will complete the platform setup
+
+        You can change the storage amount of the platform,
+        depending on what you're going to use the platform for.
+    """
+
+    # this will be the total amount of file data the platform will be able to handle
+    self.total_storage = storage_amount
+
+    # this is the new os name, assigned to os.name and sys.platform
+    if os.path.isfile(os.path.abspath('new_os_name.json')):
+      self.os_name = json.loads(open('new_os_name.json','r').read())['new_name']
+    else:
+      # os_name is the same as sys.platform
+      self.os_name = json.loads(open('new_os.json','r').read())['os_name'][1]
+
+    # re-assigning os.name and sys.platform
+    os.name= self.os_name
+    sys.platform = self.os_name
+
+    # The following updates the file of new_os_name.json to
+    # a new file name, with new data which will help
+    # support the platform later on
+    if os.path.isfile(os.path.abspath('new_os_name.json')):
+      os.remove('new_os_name.json')
+    
+    with open('new_os.json','w') as new_os:
+      # the two will be the same but we still want to store both os and sys
+      data = {'os_name':['module::os',os.name],'sys_name':['module::sys',sys.platform],'platform_available_storage':self.total_storage}
+      new_os.write(json.dumps(
+        data,
+        indent=2
+      ))
+      new_os.close()
+  
+  def _return_platform_(self):
+
+    "this will just return os.name and sys.platform if it is found in new_os.json"
+
+    open_new_os = json.loads(open('new_os.json','r').read())
+
+    if os.name == open_new_os['os_name'][1] and sys.platform == open_new_os['sys_name'][1]:
+      return (os.name if os.name == sys.platform else 'Could not return a specific platform')
+    else:
+      print('You have not ran the function _complete_platform_\nPlease run the function in order to compete platform setup')
