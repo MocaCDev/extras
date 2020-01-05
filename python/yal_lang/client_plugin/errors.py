828fc38b161439df5a32d7fc4e4684b5293c8bbb
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-01-04 18:04:34 -0600

    Update errors.py

--- python/yal_lang/client_plugin/errors.py
+++ python/yal_lang/client_plugin/errors.py
@@ -9,18 +9,24 @@ class IsAPrivateFile(Exception):
   """
 
 class SlugTimeError(TimeoutError):
-  "this will be raised if the user sets a _slug_read_ timer for over 150 seconds"
+  "this will be raised if the user sets a slug read timer for over 150 seconds"
 
   """
     The _slug_read_ function basically opens a file, and returns the result at a default time rate of 25 seconds.
     The user can change the default time rate to whatever they want, but must be under 150.
     
     NOTE:
-      The soul purpose of the _slug_read_ function is to compile the file in a fixed time rate if the size is greater
-      than 200, using os.stat().st_size, which just gets the total size of a file.
+      The soul purpose of the _slug_read_ function is to compile the file completely if it is greater than
+      200 bytes, using os.stat().st_size, which just gets the total size of a file.
 
       The time rate increases as the file gets bigger.
 
       FUNCTION:
-        def _slug_read_(self,file_name,timeout:'int'=25)
+        def _slug_read_(self,file_name,timout:'int'=25)
+  """
+
+class LessThan800StorageAmount(Exception):
+
+  """
+    This will be raised if the user assigns there platform a storage amount less than 800
   """
