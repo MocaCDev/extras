564d411f3cc95844e1a1f52305a670465886b81d
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-01-06 20:32:26 -0600

    Update __init__.py

--- python/yal_lang/__init__.py
+++ python/yal_lang/__init__.py
@@ -1,1 +1,11 @@
-version='1.0.2.1'
+"this file has not use. It is just a file that'll store the applications information"
+__version__='1.0.2.1'
+__author__='ARACADERISE'
+__type__='Programming Language'
+__compiler__='''
+  Compiles with python compiler.
+'''
+__syntax__='''
+  Uses same syntax as Python,
+  Different keywords.
+'''
