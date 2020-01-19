62cb7645daf4571a9449ede97b3498a221fa1a21
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-01-18 23:03:13 -0600

    Update file.py

--- python/yal_lang/file.py
+++ python/yal_lang/file.py
@@ -1,15 +1,28 @@
 import os
 from lang import read_
 from easier import yal
+from time import sleep
 
-# we want the platform to be the YAL_ORIGINAL for the compilation of the .yal file
-yal('AOP')
 
-# we want to render all the .yal files
+yal = yal()
+
+yal._open_file_('data.txt')
+
+yal._setup_platform_('YAL_ORIGINAL')
+
+yal._yal_filenames_()
+
 yal._render_yal_files_()
+
+yal._render_path_(path=['templates/keep_track_of_import.txt','d.txt','d.json','t'])
+
+if yal.is_a_rendered_path(check=['d.txt','d.json']):
+  yal._return_rendered_(5)
+sleep(10)
 
 os.system('clear')
 
+main_path = os.environ.get('HOME')
 path = os.getcwd().replace('/extras/python/yal_lang','')
 os.chdir(path)
 get_file = []
