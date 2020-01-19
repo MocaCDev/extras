eaf6bd362ba8642d2d78c7aa829b5a8cf7a54f4f
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-01-18 23:41:05 -0600

    Update errors.py

--- python/yal_lang/client_plugin/errors.py
+++ python/yal_lang/client_plugin/errors.py
@@ -8,6 +8,16 @@ class IsAPrivateFile(Exception):
       There will be a Yal Application compiler that compiles the locked file
   """
 
+class NotEnoughStorage(OverflowError):
+  """
+    This will be raised if the user is taking more storage out than what was assigned to the platform
+  """
+
+class CouldNotLocateFile(FileNotFoundError):
+  """
+    This will be raised if the yal application cannot find a file
+  """
+
 class SlugTimeError(TimeoutError):
   "this will be raised if the user sets a slug read timer for over 50 seconds"
 
@@ -19,10 +29,23 @@ class SlugTimeError(TimeoutError):
       The soul purpose of the _slug_read_ function is to compile the file completely if it is greater than
       200 bytes, using os.stat('FILENAME').st_size, which just gets the total size of a file.
 
+      The timeout will take place after a default time of 10 seconds. You can change this by adding the argument:
+        second_til_timout=Your Number Here
+
       The time rate increases as the file gets bigger.
 
       FUNCTION:
-        def _slug_read_(self,file_name,timout:'int'=5)
+        def _slug_read_(self,file_name,second_til_timout:'int'=10,timout:'int'=5)
+  """
+
+class CannotReadPdfFileError(Exception):
+  """
+  this will be raised if the user tries to read a .pdf file
+  """
+
+class CannotReadImageFileError(Exception):
+  """
+  This will be raised if the user tries to read a .img, .png, etc, type of file.
   """
 
 class LessThan800StorageAmount(Exception):
