86beaedb347f1bb73fa329394016353109cd62f2
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-01-20 15:31:52 -0600

    Update main.cs

--- C#/getting_back/main.cs
+++ C#/getting_back/main.cs
@@ -1,13 +1,15 @@
 using System;
 
-public class main {
-  public static void Main() {
-    String my_line = "Getting back into C#";
-    bool is_getting_back = my_line == "Getting back into C#";
+namespace MyFirstProjectBackIntoC# {
+  public class main {
+    public static void Main() {
+      String my_line = "Getting back into C#";
+      bool is_getting_back = my_line == "Getting back into C#";
 
-    if(is_getting_back)
-      Console.WriteLine(my_line);
-    else
-      Console.WriteLine("Too bad, I'm still getting into it");
+      if(is_getting_back)
+        Console.WriteLine(my_line);
+      else
+        Console.WriteLine("Too bad, I'm still getting into it");
+    }
   }
 }
