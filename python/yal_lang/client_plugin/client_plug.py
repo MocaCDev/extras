aa68482665aadb5134de302130430df109b482af
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-01-27 21:52:36 -0600

    Update client_plug.py

--- python/yal_lang/client_plugin/client_plug.py
+++ python/yal_lang/client_plugin/client_plug.py
@@ -14,6 +14,9 @@ class yal_client_plugin:
 
   def _complete_platform_(self,storage_amount:'int'=500000) -> list([int,[str,str]]):
 
+    # !IMPORTANT
+    self.platform_is_rendered = False
+
     """this will complete the platform setup
 
         You can change the storage amount of the platform,
@@ -59,7 +62,45 @@ class yal_client_plugin:
       new_os.close()
     return [self.total_storage,[os.name,sys.platform]]
   
+  def _render_platform_(self,render_to_host:str,connection_available:bool) -> list([str,bool]):
+
+    "this will render the platform and set a host and a connection avaiability variable to it"
+
+    self.render_host = render_to_host
+    self.connection_available = connection_available
+
+    data = {'platform':os.name,'platform_renderes_to_host':self.render_host,'available':self.connection_available}
+
+    if self.connection_available:
+      data.update({['Connection is unavailable. Other hosts and or platform names cannot connect to it through the yal application. You must creat a mod of your own if you want to connect to it.']})
+
+    with open('render_platform.json','w') as render_plat_data:
+
+      render_plat_data.write(json.dumps(
+        data,
+        indent=2,
+        sort_keys=False
+      ))
+      render_plat_data.flush()
+      render_plat_data.close()
+    
+    self.platform_is_rendered = True
+
+    self.rendered_msg = "<RENDER PLATFORM:{0},\nsaved:{1},\nsaved_to_file:{2}"
+
+    with open('rendered_info.bach','w') as file:
+
+      self.rendered_msg = self.rendere_msg.format([os.name,sys.platform],True if self.platform_is_rendered else False,os.path.abspath('rendered_info.bach'))
+
+      file.write(self.rendere_msg)
+      file.close()
+    
+    return [self.rendered_msg,self.platform_is_rendered]
+  
   def _update_storage_(self,take_out:'int') -> int:
+
+    if not self.platform_is_rendered:
+      raise Exception('\n\nCannot add storage to a platform that has not been rendred\n\nPlease run yal._render_platform_ to render the platform')
 
     """
       This function will TAKE OUT from the platform_available_storage
@@ -83,6 +124,9 @@ class yal_client_plugin:
   
   def _return_storage_amount_(self) -> int:
 
+    if not self.platform_is_rendered:
+      raise Exception('\n\nCannot return the storage ammount of a platform that has not been rendered\n\nPlease run yal._render_platform_ to render the platform.')
+
     """
       This will be used to return how much storage is left
     """
@@ -93,6 +137,9 @@ class yal_client_plugin:
     return check_file['platform_available_storage'],print(Fore.CYAN + Style.BRIGHT + 'Storage Amount: ' + str(check_file['platform_available_storage']))
   
   def _return_platform_(self):
+
+    if not self.platform_is_rendered:
+      raise Exception('\n\nCannot return the platorm of a platform that has not been rendered\n\nRun yal._render_platform_ to render the platform.')
 
     """this will just return os.name if the path new_os.json exists, which just means the platform has been setup completely
         
@@ -107,6 +154,10 @@ class yal_client_plugin:
     )
   
   def _use_platform_(self) -> Tuple[str]:
+
+    if not self.platform_is_rendered:
+      raise Exception('\n\nCannot use a platform that has not been rendered\n\nRun yal._render_platform_ to render the platform.')
+
     """this just returns the name in new_os.json if the path exists
         
         It is the same as _return_patform_, but it should be used when re-assigning os.name or sys.platform in your .py file
@@ -117,6 +168,9 @@ class yal_client_plugin:
     else:return (os.name)#will re-assing os.name to os.name(same value), as well as sys.platform
   
   def _add_storage_(self,add):
+
+    if not self.platform_is_rendered:
+      raise Exception('\n\nCannot add storage to a platform that has not been rendered\n\nRun yal._render_platform_ to render the platform.')
 
     """
       this will add more storage to the platform
