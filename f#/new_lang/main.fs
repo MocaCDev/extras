7bea54a5af772dd99df93655da134a4a8ab6e5c6
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-02-18 21:41:16 -0600

    Update main.fs

--- f#/new_lang/main.fs
+++ f#/new_lang/main.fs
@@ -1,6 +1,6 @@
 open System
 
-let num n = if n > 10 && n < 20 then "greater than 10, less than 20" else "idk"
+let num n = if n >= 10 && n <= 20 then "greater, equal to 10, less, equal to 20" else "idk"
 
 let main() =
   Console.WriteLine(num 10)
