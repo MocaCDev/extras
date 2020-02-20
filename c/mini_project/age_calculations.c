6b573f20426c033b4c8c837ed8fdcb4f687dc50f
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-02-20 16:54:03 -0600

    Update age_calculations.c

--- c/mini_project/age_calculations.c
+++ c/mini_project/age_calculations.c
@@ -15,15 +15,19 @@ int main() {
     scanf("%d",&years_old);
     int total_months = months*years_old;
     int lead_back = total_months/months;
+
+		bool above = total_months > 800 ? true : false;
     
-    printf("You have lived %d months.\nEquation: %d*%d=%d\n%d/%d=%d\n\n",total_months,months,years_old,total_months,total_months,months,lead_back);
+    printf("You have lived %d months.\nEquation: %d*%d=%d\n%d/%d=%d\n%s\n",total_months,months,years_old,total_months,total_months,months,lead_back,above == true ? "Quite a bit of months!\n" : "eh, quite a bit of months. Still got a bit to go\n");
     
     // we don't want there to be any decimal places, so we are going to keep it as (int)
     avg = (int) day_in_years/months;
     int your_total_days = avg*total_months;
     int leading_back = your_total_days/total_months;
+
+		bool abot_ = your_total_days > 25000 ? true : false;
     
-    printf("There are approx. %.0f days in a month.\nMeaning you have lived for %d days\nNevertheless, %d/%d=%d. 30 being the average amount of days in a month\n\nThis is being said by %s in the c language\n",avg,your_total_days,your_total_days,total_months,leading_back,name);
+    printf("There are approx. %.0f days in a month.\nMeaning you have lived for %d days\nNevertheless, %d/%d=%d. 30 being the average amount of days in a month\n%s\nThis is being said by %s in the c language\n",avg,your_total_days,your_total_days,total_months,leading_back,abot_ == true ? "Allot of days\n" : "eh, quite a bit of days. Still young ;)\n",name);
     
     return 0;
 }
