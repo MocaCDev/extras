ebf2162fdc0cdc12f616da7b84837bc58d99b5ec
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-03-17 13:53:02 -0500

    Create CHK.h

---
+++ c/advancing/CHK.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define exists_code 10
+#define can_be_created 42
+
+int file_exists(char *file_name, char type[2]);
+char * fstatus(void);
+int pfstatus(void);
