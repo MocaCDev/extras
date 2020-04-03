d976ec774676a3fae1bb673f645dbfc10233b4c6
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-04-02 23:18:34 -0500

    Create more.js

---
+++ javascript/LEARNING/otherStuff/more.js
@@ -0,0 +1,18 @@
+let list = {
+	crewInfo: {
+		crewLeft (arg) {
+			console.log(arg*10);
+		}
+	},
+	list_:[1,2,3,4,5,6]
+};
+
+list.list_ = list.list_.filter(index => {
+	return index>=4;
+});
+list.list_ = list.list_.map(index => {
+	return index*2;
+});
+
+const remainderIsZero = argument => (argument%2==0)?console.log(`${argument}%2=${argument%2}->${true}`):console.log(`${argument}%2=${argument%2}->${false}`);
+const check = list.list_.every(remainderIsZero);
