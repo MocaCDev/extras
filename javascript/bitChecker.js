ae8109b09b374939f9b8e5912ec1be4dbc666ed9
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-04-04 20:30:50 -0500

    Update bitChecker.js

--- javascript/bitChecker.js
+++ javascript/bitChecker.js
@@ -1,15 +1,16 @@
 let available16BitNumbers = [];
 let available24BitNumbers = [];
+let FailedAttempts = [{'Failed int 16 check':[]},{'Failed int 24 check':[]}];
 
 // takes a int and sees if it is greater than a int16
 const uInt16 = (numbers) => {
 	let ReturnStatus;
 	for(let i = 0; i < numbers.length; i++) {
 		if(Math.floor(Math.log2(numbers[i])) > 16) {
-			console.log('\033[0;31m'+`The number ${numbers[i]} is not 16 bits long instead it is ${Math.floor(Math.log2(numbers[i]))}`+'\033[0;0m');
-			 ReturnStatus=1;
+			numbers[i]=Math.abs(numbers[i]);
+			FailedAttempts[0]['Failed int 16 check'].push(numbers[i]);
+			ReturnStatus=1;
 		} else {
-			console.log('\033[0;32m'+`The number ${numbers[i]} is ${Math.floor(Math.log2(numbers[i]))} bits long`);
 			available16BitNumbers.push(numbers[i]);
 			ReturnStatus=0;
 		}
@@ -21,18 +22,25 @@ const uInt16 = (numbers) => {
 const uInt24 = (numbers) => {
 	let ReturnStatus;
 	for(let i = 0; i < numbers.length; i++) {
-		if(Math.floor(Math.log2(numbers[i])) > 24) {
-			console.log('\033[0;43m'+`The number ${numbers[i]} is not 24 bits long instead it is ${Math.floor(Math.log2(numbers[i]))}`+'\033[0;0m');
+		numbers[i]=Math.abs(numbers[i]);
+		if(Math.floor(Math.log2(numbers[i])) > 24 || Math.floor(Math.log2(numbers[i])) < 16) {
+			FailedAttempts[1]['Failed int 24 check'].push(numbers[i]);
 			ReturnStatus=1;
 		} else {
-			console.log('\033[0;32m'+`The number ${numbers[i]} is ${Math.floor(Math.log2(numbers[i]))} bits long`);
 			available24BitNumbers.push(numbers[i]);
 			ReturnStatus=0;
 		}
 	}
 }
 
-uInt16([10000,100,50,9000,180000]);
+uInt16([10000,100,50,9000,180000,1900000]);
+uInt24([1000000,800000,-1,5000,40,2,1]);
+
 for(let d = 0; d < available16BitNumbers.length; d++) {
 	console.log('Picked up on: '+`${available16BitNumbers[d]}`);
+}
+for(let x = 0; x < FailedAttempts.length; x++) {
+	console.log('\n\033[3;33mFailed 16 bit checks: '+FailedAttempts[x]['Failed int 16 check']);
+	console.log('\033[3;33mFailed 24 bit checks: '+FailedAttempts[x+1]['Failed int 24 check']);
+	break;
 }
