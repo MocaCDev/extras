5fd3af1866e2cdddcf7db267547440a8396749f9
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-04-11 01:51:13 -0500

    Update secondWay.kt

--- kotlin/secondWay.kt
+++ kotlin/secondWay.kt
@@ -3,7 +3,15 @@
 var MyName: String? = null
 
 fun main() {
+    // Using if
     val NAME = if(MyName != null) MyName else "ARACADERISE(GetStuffDone)"
     
-    println(NAME + " is the best 15 year old developer")
+    // Using when
+    val NAME_ = when(MyName) {
+        null -> "ARACADERISE(GetStuffDone)"
+        else -> MyName
+    }
+    
+    print(NAME + " is the best 15 year old developer\n") // print doesn't create new line, \n needed
+    print(NAME_ + " is the best 15 year old developer")
 }
