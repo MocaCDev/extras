751e29d8d03cc6018183c04e8cd44acc2b76948d
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-04-11 02:58:09 -0500

    Update arrays.kt

--- kotlin/arrays.kt
+++ kotlin/arrays.kt
@@ -5,8 +5,12 @@ fun main() {
     myArr.add(5)
     val anotherArr = mutableListOf("ARACADERISE","(GetStuffDone)")
     
+    // MAPS
+    val MAP = mutableMapOf(1 to "fail", 0 to "success")
+    
     anotherArr.forEachIndexed{ index, _ -> Name+=anotherArr[index]}
     println(Name)
+    MAP.forEach{ token, value -> println("$token = $value")}
     
     // This is one way
     for(ITEM in myArr) {
