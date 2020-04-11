d809d096fe043d60dfe2e1c7d66fdfcd265195c4
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-04-11 02:55:59 -0500

    Update arrays.kt

--- kotlin/arrays.kt
+++ kotlin/arrays.kt
@@ -1,5 +1,12 @@
+var Name: String? = null
+
 fun main() {
-    val myArr = arrayOf(1,2,3,4)
+    val myArr = mutableListOf(1,2,3,4)
+    myArr.add(5)
+    val anotherArr = mutableListOf("ARACADERISE","(GetStuffDone)")
+    
+    anotherArr.forEachIndexed{ index, _ -> Name+=anotherArr[index]}
+    println(Name)
     
     // This is one way
     for(ITEM in myArr) {
