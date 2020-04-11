2d4dff4d515783592ccb75581822bedd21238633
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-04-11 01:44:52 -0500

    Update firstKotlin.kt

--- kotlin/firstKotlin.kt
+++ kotlin/firstKotlin.kt
@@ -1,10 +1,13 @@
 var MyName: String? = null
 
 fun main() {
-    MyName = "AIDAN"
+    MyName = "ARACADERISE"
+    if(MyName == "ARACADERISE") {
+        MyName+="(GetStuffDone)"
+    }
     when(MyName) {
         null -> println("IS NULL")
-        "AIDAN" -> println("IS AIDAN")
+        "ARACADERISE(GetStuffDone)" -> println(MyName+" is the best 15 year old developer!")
         else -> println(MyName)
     }
 }
