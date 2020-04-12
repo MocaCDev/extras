60a51e17b812b6ddd2f962fffd25e6ad33a71558
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-04-12 13:50:23 -0500

    Update memset.c

--- c/memset.c
+++ c/memset.c
@@ -6,23 +6,23 @@
 static int Index;
 
 typedef struct {
-    char STRING_TO_SAVE[100][MAX_STRING_SIZE];
+    char STRING_TO_SAVE[400][MAX_STRING_SIZE];
+    char SAVE_TOGETHER[1000];
 } SaveString;
 
-char * GET(char str[], SaveString *String) {
+char * GETCAT(char str[], SaveString *String) {
     static char NEW_STRING[MAX_STRING_SIZE];
     if(!(strlen(str)>MAX_STRING_SIZE)) {
-        memset(NEW_STRING,'_',strlen(str)+2); // Adding 2 to it so we have 5 left over underscores
+        memset(NEW_STRING,'_',strlen(str)+3); // Adding 2 to it so we have 5 left over underscores
         for(int i = 0; i < strlen(str); i++) {
             memset(&NEW_STRING[i],str[i],1);
         }
         strcpy(String->STRING_TO_SAVE[Index],NEW_STRING);
-        memset(NEW_STRING,'_',sizeof(char*)); // Setting it back to '_' of length char*
-        return String->STRING_TO_SAVE[Index];
+        Index++;
+        return NEW_STRING;
     } else {
        sprintf(NEW_STRING,"CANNOT GET STRING OF SIZE GREATER THEN %d",MAX_STRING_SIZE);
     }
-    Index++;
     return NEW_STRING;
 }
 
@@ -30,11 +30,27 @@ int main(void) {
     // Local Ideals
     SaveString *String = (SaveString *) malloc(sizeof(SaveString));
     
-    // Getting input
-    char ITEM[MAX_STRING_SIZE];
-    printf("INPUT: ");
-    scanf("%s",ITEM);
+    // Getting ammount of times
+    int AmTime;
+    printf("Ammount to get: ");
+    scanf("%d",&AmTime);
     
-    // Printing it
-    puts(GET(ITEM,String));
+    // Getting input for ammount of AmTime
+    char ITEM[MAX_STRING_SIZE];
+    if(!(AmTime>399)) { 
+        for(int i = 0; i < AmTime; i++) {
+            printf("INPUT: ");
+            scanf("%s",ITEM);
+            
+            // getting it
+            GETCAT(ITEM,String);
+        }
+    }
+    
+    for(int i = 0; i < sizeof(String->STRING_TO_SAVE)/sizeof(String->STRING_TO_SAVE[0]); i++) {
+        if(!(strcmp(String->STRING_TO_SAVE[i],"")==0))strcat(String->SAVE_TOGETHER,String->STRING_TO_SAVE[i]);
+        else break;
+    }
+    
+    printf("Done!\n%s",String->SAVE_TOGETHER);
 }
