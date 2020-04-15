7127ec198dfd3daa25f315bd32e1be205019fce4
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-04-15 17:41:09 -0500

    Update script.js

--- htmlProjectsWithJs/script.js
+++ htmlProjectsWithJs/script.js
@@ -3,69 +3,81 @@ let Increment = prompt("Increment By: ");
 let FoundNumbers = []; // Appending numbers found
 let ErrorNumber;
 let IncrementEach;
-if (Increment) {
-    IncrementEach = parseInt(Increment);
+
+if(Increment) {
+  IncrementEach=parseInt(Increment);
 } else {
-    IncrementEach = 1; // DEFAULT
+  IncrementEach=1; // DEFAULT
 }
-if (UserInput === "") {
-    document.getElementById("NoInput").innerHTML = "Clicked \"OK\".</br>User did not input anything".fontcolor('white').bold().fixed();
-    throw new Error("No input");
-} else if (UserInput) {
-    UserInput += ' ';
-    document.getElementById("userInput").innerHTML = `User input: ${UserInput}`.bold().fixed();
-    document.getElementById('IncrementBy').innerHTML = `</br>Incrementing by: ${IncrementEach}`.bold().fixed();
-    for (let i = 0; i < UserInput.length; i++) {
-        if (UserInput[i] == ' ') {
-            if (UserInput[i - 2] != ' ' && UserInput[i - 1] != ' ') {
-                if (UserInput[i - 2] != undefined) {
-                    let Number = UserInput[i - 2] + UserInput[i - 1];
-                    FoundNumbers.push(parseInt(Number));
-                } else {
-                    /* Since i-2 is undefined i-1 should be a numbers */
-                    ErrorNumber = UserInput[i - 1];
-                }
-            } else {
-                /*
-                  We could've just left:
-                    FoundNumbers.push(parseInt(ErrorNumber))
-                  without the if statement, but it is more efficient if we do it this way
-                */
-                if (ErrorNumber != null) {
-                    FoundNumbers.push(parseInt(ErrorNumber));
-                }
-                FoundNumbers.push(parseInt(UserInput[i - 1]));
-            }
+
+if(UserInput==="") {
+  document.getElementById("NoInput").innerHTML = "Clicked \"OK\".</br>User did not input anything".fontcolor('white').bold().fixed();
+  throw new Error("No input");
+} else if(UserInput) {
+  UserInput += ' ';
+
+  document.getElementById("userInput").innerHTML = `User input: ${UserInput}`.bold().fixed();
+  document.getElementById('IncrementBy').innerHTML = `</br>Incrementing by: ${IncrementEach}`.bold().fixed();
+
+  for(let i = 0; i < UserInput.length; i++) {
+    if(UserInput[i]==' ') {
+      if(UserInput[i-2]!=' '&&UserInput[i-1]!=' ') {
+        if(UserInput[i-2]!=undefined) {
+          let Number = UserInput[i-2]+UserInput[i-1];
+          FoundNumbers.push(parseInt(Number));
+        } else {
+          /* Since i-2 is undefined i-1 should be a numbers */
+          ErrorNumber = UserInput[i-1];
         }
+      } else {
+        /*
+          We could've just left:
+            FoundNumbers.push(parseInt(ErrorNumber))
+          without the if statement, but it is more efficient if we do it this way
+        */
+        console.log(parseInt(UserInput[i-1]));
+        if(ErrorNumber!=null) {
+          FoundNumbers.push(parseInt(ErrorNumber));
+        }
+        FoundNumbers.push(parseInt(UserInput[i-1]));
+      }
     }
+  }
 } else {
-    document.getElementById("NoInput").innerHTML = "Clicked \"CANCEL\"</br>User did not input anything".fontcolor('white').bold().fixed();
-    throw new Error("No input");
+  document.getElementById("NoInput").innerHTML = "Clicked \"CANCEL\"</br>User did not input anything".fontcolor('white').bold().fixed();
+  throw new Error("No input");
 }
+
 // Sorting numbers
 FoundNumbers.sort();
+
 // Remving NaN from the list
-for (let i = 0; i < FoundNumbers.length; i++) {
-    if (isNaN(FoundNumbers[i])) {
-        FoundNumbers[i] = 0;
-    }
+for(let i = 0; i < FoundNumbers.length; i++) {
+  if(isNaN(FoundNumbers[i])) {
+    FoundNumbers[i]=0;
+  }
 }
+
 document.getElementById('ListDuringParse').innerHTML = `</br>List During parse: [${FoundNumbers}](SORTED)`.bold().fixed();
+
 // Removing duplicate numbers
-for (let i = 0; i < FoundNumbers.length; i++) {
-    if (FoundNumbers[i] == FoundNumbers[i + 1]) {
-        document.getElementById('Removed').innerHTML = `</br>Removed items: ${FoundNumbers[i]}`.bold().fixed();
-        FoundNumbers[i] = 0;
-    }
+for(let i = 0; i < FoundNumbers.length; i++) {
+  if(FoundNumbers[i]==FoundNumbers[i+1]){
+    document.getElementById('Removed').innerHTML = `</br>Removed items: ${FoundNumbers[i]}`.bold().fixed();
+    FoundNumbers[i] = 0;
+  }
 }
+
 FoundNumbers = FoundNumbers.filter(index => {
-    return index > 0;
+  return index > 0;
 });
+
 FoundNumbers = FoundNumbers.map(index => {
-    return index + IncrementEach;
+  return index + IncrementEach;
 });
-if (FoundNumbers[0] != null) {
-    document.getElementById('output').innerHTML = `</br><hr style='width:50%'>Output: ${FoundNumbers}`.fontcolor('cyan').bold().fixed();
+
+if(FoundNumbers[0]!=null) {
+  document.getElementById('output').innerHTML = `</br><hr style='width:50%'>Output: [${FoundNumbers}]`.fontcolor('darkred').bold().fixed();
 } else {
-    document.getElementById('output').innerHTML = `</br><hr style='width:50%'>Cannot take in ${UserInput}. Too large`.fontcolor('cyan').bold().fixed();
+  document.getElementById('output').innerHTML =  `</br><hr style='width:50%'>Cannot take in ${UserInput}. Too large`.fontcolor('darkred').bold().fixed();
 }
