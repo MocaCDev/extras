649768dd12064f3664b7c2112f2a693cf4230206
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-04-16 02:17:44 -0500

    Update AMAZING.js

--- htmlProjectsWithJs/AMAZING.js
+++ htmlProjectsWithJs/AMAZING.js
@@ -10,7 +10,11 @@ class MakeMiniApi {
   }
 
   PRINT() {
-    // Removing 0 in UserInp, the list has not yet been modified
+    /*
+      Removing 0 in UserInp, the list has not yet been modified
+      Checking if type is string. If user inputs nothing we set each ideal of the class
+      to 'No Input' which is a string
+     */
     if(typeof this.UserInp[0]!='string') {
       this.UserInp = this.UserInp.filter(index => index>0);
     }
@@ -37,9 +41,26 @@ $(function(){
     console.log(API.PRINT());
   } else {
     API = new MakeMiniApi('No Input','No Input','No Input');
-    console.log(API.PRINT());
     document.getElementById('GetInfo').onclick = function(){
       document.getElementById('ShowInfo').innerHTML = `NO INFORMATION FOUND</br>{SORTED_INPUT:'${API.PRINT().SORTED_INPUT}',OUTPUT:'${API.PRINT().OUTPUT}',INCREMEMT_BY:'${API.PRINT().INCREMENT_BY}'}`.fontcolor('red').fixed().big();
+
+      // Creating a "Hide Information" button
+      let NewBtn = document.createElement('button');
+      NewBtn.className='button';
+      let Information = document.createTextNode('Hide Information');
+      NewBtn.appendChild(Information);
+      let SpanElement = document.getElementById('HideBtn');
+      SpanElement.appendChild(NewBtn);
+      console.log('DONE');
+
+      document.getElementById('HideBtn').onclick = function(){
+        document.getElementById('ShowInfo').innerHTML='';
+        
+        let REMOVE = document.getElementById('GetInfo');
+        let REMOVE2 = document.getElementById('HideBtn');
+        REMOVE.remove();REMOVE2.remove();
+      }
     }
+    console.log(API.PRINT());
   }
 });
