481c4ea31bdc90cbacbe3a1e3445d0f9903a5fb7
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-04-20 00:21:14 -0500

    Update and rename sayHiARACADE.go to SOM.go

---
+++ GO!!/NEW/SOM.go
@@ -0,0 +1,7 @@
+package main
+
+import f"fmt"
+
+func SayThis() {
+	f.Println("\nPROGRAM DONE")
+}
--- GO!!/NEW/sayHiARACADE.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import f"fmt"
-
-func SayHi() {
-  f.Println("ARACADERISE is now learning GO!!!")
-}
