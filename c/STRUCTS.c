b1c4904c0a2f21605780e0ffe13e89807b77be95
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-04-22 01:36:00 -0500

    Update STRUCTS.c

--- c/STRUCTS.c
+++ c/STRUCTS.c
@@ -12,7 +12,8 @@ struct Decide {
     depending on SIZE.
 */
 struct Decide *SetupStructSize(struct Decide *d, int SIZE) {
-    d = malloc(sizeof(*d) + SIZE);
+    
+    d = malloc(sizeof(*d)+SIZE);
     
     if((sizeof(*d)+SIZE)>999&&(sizeof(*d)+SIZE<4000)) {
         SIZE/=floor(550);
@@ -21,8 +22,14 @@ struct Decide *SetupStructSize(struct Decide *d, int SIZE) {
         if((sizeof(*d)+SIZE)>4000) {
             SIZE/=floor(1050);
             // Making size relevant
-            if(SIZE<200) {
+            if(SIZE<200&&SIZE>0) {
                 SIZE*=20;
+            } else if(SIZE>1200) {
+                SIZE/=200;
+                // Having to check again..just in case :) (P.S: I am the CEO of repeating if statements)
+                if(SIZE<200) {
+                    SIZE*=20;
+                }
             }
             d->Size=(sizeof(*d)+SIZE/2);
         } else if((sizeof(*d)+SIZE)<999&&(sizeof(*d)+SIZE)>0) {
@@ -33,11 +40,14 @@ struct Decide *SetupStructSize(struct Decide *d, int SIZE) {
         }
     }
     
+    // Reallocating memory block based off of d->Size
+    d = realloc(d,sizeof(*d)+d->Size);
+    
     return d;
 }
 
 int main() {
-    struct Decide *D = SetupStructSize(D,999888);
+    struct Decide *D = SetupStructSize(D,900);
     
-    printf("%d",D->Size);
+    printf("Saved %d bytes for each...",D->Size);
 }
