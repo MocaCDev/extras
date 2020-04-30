6b6887c651b47be5688015fe906809bd6a0963cd
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-04-30 01:05:06 -0500

    Update main.c

--- c/BYTES/main.c
+++ c/BYTES/main.c
@@ -7,24 +7,24 @@ int main(int a, char **args) {
 
 	PARSE_ARGS(a, args);
 
-    char STRING[MAX_STRING_LENGTH];
-    int *Bytes;
-    int BYTES;
+    	char STRING[MAX_STRING_LENGTH];
+    	int *Bytes;
+    	int BYTES;
 
 	/*New way of creating arrays!*/
 	mIntBitArr(900, COUNT);
 	mIntAssign(900, 40);
     
-    // Getting String
-    printf("STRING: ");
-    fgets(STRING,MAX_STRING_LENGTH,stdin);
+    	// Getting String
+    	printf("STRING: ");
+    	fgets(STRING,MAX_STRING_LENGTH,stdin);
     
-    MakeByte(STRING,Bytes,true,BYTES);
-    MemoryAbstract(BYTES,-1000,Bytes,true);
+    	MakeByte(STRING,Bytes,true,BYTES);
+    	MemoryAbstract(BYTES,-1000,Bytes,true);
 }
 
 int
 len(char *String) {
-    int length=strlen(String);
-    return length;
+    	int length=strlen(String);
+    	return length;
 }
