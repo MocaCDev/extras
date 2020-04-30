ca09b4b778958d0ce86c95183dd873ad9a0add6b
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-04-30 01:04:46 -0500

    Update main.c

--- c/BYTES/main.c
+++ c/BYTES/main.c
@@ -5,15 +5,15 @@ len(char *String);
 
 int main(int a, char **args) {
 
-		PARSE_ARGS(a, args);
+	PARSE_ARGS(a, args);
 
     char STRING[MAX_STRING_LENGTH];
     int *Bytes;
     int BYTES;
 
-		/*New way of creating arrays!*/
-		mIntBitArr(900, COUNT);
-		mIntAssign(900, 40);
+	/*New way of creating arrays!*/
+	mIntBitArr(900, COUNT);
+	mIntAssign(900, 40);
     
     // Getting String
     printf("STRING: ");
