ef11890480740044be185e44e2bb43c0826a7653
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-05-21 16:39:15 -0500

    Update sayHello.asm

--- assembly/sayHello.asm
+++ assembly/sayHello.asm
@@ -27,14 +27,32 @@ section .text
         push_: push 0x00
         
         addByte:
+            ; Removing from stack..
+            pop eax
+            pop ecx
+            pop edx
+            pop ebx
+            
+            ; Pushing new element of 0x00 to stack after removing eax,ecx,edx and ebx
+            call push_
+            
+            ; Assigning 4 bytes to esp(top of stack), and ebp(base of stack) registers.
             mov BYTE [esp], 32
             mov BYTE [ebp], 32
+        
+        ; Adding bytes to esp and ebp registers(working with the stack here..)
         call addByte
         
+        ; Assigning values to each
         mov [ebp], edx
+        mov [esp], ebx
         
+        ; re-calling push_ to push onto the stack
+        ; adding bytes to new stack element
+        ; calling the kernel(interrupt call, 80h, 0x80..)
         call push_
         call addByte
-        
         call kernCall
+        
+        ; returning, and exiting program..
         ret
