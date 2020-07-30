0efbcd8569dba48aaa2da5b8688547ebb49ce9c5
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-07-30 18:47:31 -0500

    Create helloworld.adb

---
+++ Ada/helloworld.adb
@@ -0,0 +1,21 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+
+procedure HelloWorld is
+    N: Integer;
+begin
+    Put("Enter a number: ");
+    Get(N);
+    if N < 10 or N = 55 then
+        if N = 55 then
+            Put("Number N is");
+            Put(N);
+            New_Line;
+        else
+            Put("N is");
+            Put(N);
+        end if;
+    else
+        Put(N);
+    end if;
+end HelloWorld;
