d31c76c336ff9facdb48ddb259b2f08a7d850fe8
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-10-27 14:44:31 -0500

    Update helloWorld.s

--- assembly/helloWorld.s
+++ assembly/helloWorld.s
@@ -1,25 +1,44 @@
 section .text
-	global _start
-	global action
-	%define system_call int 0x80
+    global _start
+    global print
+    global action
+    extern printf
+    
+    %define system_call int 0x80
+    
+    action:
+        ; setting up the "Hello World" variable
+        mov ecx, msg
+        mov edx, len
+        mov eax, 4
+        mov ebx, 1
+        
+        ;call printf
+        system_call
+        
+        mov ecx, msg2
+        mov edx, len2
+        mov eax, 4
+        mov ebx, 1
+        system_call
+        ret
+    
+    print:
+        msg db "Hello World", 0xa
+        len equ $ - msg
+        
+        msg2 db "Hello world 2", 0xa
+        len2 equ $ - msg2
+        ret
+    
+    _start:
+        
+        call action
+        
+        
+        mov eax, 1
+        mov ebx, 0
+        system_call
 
-action:
-	mov ecx, msg
-	mov edx, len
-	mov eax, 4
-	mov ebx, 1
-	system_call
-	
-	ret
-
-_start:
-
-	call action
-	
-	mov eax, 1
-	mov ebx, 0
-	system_call
-
-section .data
-	msg db "Hello World"
-	len equ $ - msg
+segment .rodata
+    call print
