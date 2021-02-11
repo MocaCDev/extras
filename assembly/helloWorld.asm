2afa847c572be26092649884475cbfff58995b24
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2021-02-11 08:38:53 -0600

    Update helloWorld.asm

--- assembly/helloWorld.asm
+++ assembly/helloWorld.asm
@@ -1,51 +1,48 @@
 section .text
     global _start
     %define sys_call int 0x80
-    %macro stdin 2
-        mov eax, 3 ; read
-        mov ebx, 0 ; stdin
-        mov ecx, %1
-        mov edx, %2+%2-1 ; Add the total length of the string by the length of the screen subracted by one(removing byte for the \0)
-        push ecx
+    
+    %macro stdflush 0
+        mov ecx, '0'
+        mov eax, '0'
+        mov ebx, '0'
+        sys_call
+        
+        pop ecx ; remove value of ecx, not-final
+        mov ebp, buffer ; push the buffer onto the stack, which is what ecx was holding.
+                        ; only difference is that the buffer is the final value of whatever ecx had.
         sys_call
     %endmacro%
-    %macro stdout 2
-        mov ecx, %2
-        mov esi, input
-        mov edi, string
-        cld
-        rep movsb
+    
+    %macro stdin 2
+        mov eax, 3
+        mov edx, %2+%2-1
+        mov ecx, buffer
+        mov ebx, 0 ; stdin
         sys_call
         
-        mov ecx, string
-        mov ebx, %2
-        mov eax, 4 ; output
-        mov ebx, 1 ; stdout
-        sys_call
-        
-        pop ecx
-        push string
-        sys_call
-        
+        push ecx ; ecx will have the buffer. Push this onto the stack, and the flush it with stdflush.
         push ebp
-        mov ebp, esp ; re-initialize
+        mov ebp, esp
         sys_call
+        
+        stdflush
     %endmacro%
 
 _start:
 
-    push ebp
-    mov ebp, esp
+    stdin buffer, len
     
-    stdin input, len
-    
-    stdout input, len
+    mov ecx, buffer
+    mov edx, len
+    mov eax, 4
+    mov ebx, 1
+    sys_call
     
     mov eax, 1
     mov ebx, 0
     sys_call
 
 segment .bss
-    input resq 5
-    len equ $ - input
-    string resq 5
+    buffer resq 5
+    len equ $ - buffer
