540e3cdd8c269ee515eaba1bac38b7be2e08328f
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2021-04-11 13:19:03 -0500

    Update img.c

--- c/img.c
+++ c/img.c
@@ -21,8 +21,8 @@ int configure_height(int width, int l)
 void create_image(FILE* file, char *colors, int length, int width, int dimmed)
 {
 	char info_header[  ] = { 
-    'B', 'M'
-  };
+    		'B', 'M'
+  	};
 
 	int height = configure_height(width, length);
 	int paddedw = r4(width);
@@ -37,23 +37,23 @@ void create_image(FILE* file, char *colors, int length, int width, int dimmed)
 		0x104e23, 0x104e23, 0, 0 // high resolution
 	};
 
-  unsigned char *padding = (unsigned char*)malloc(paddedw * 3 * width * sizeof(*padding));
+  	unsigned char *padding = (unsigned char*)malloc(paddedw * 3 * width * sizeof(*padding));
 
 	header[0] = sizeof(info_header) + sizeof(header) + size;
 
 	char* image = (char*)malloc(size * sizeof(char*));
 
-  for(int i = 0; i < paddedw; i++) // padded width for every 3 bytes of the image
-  {
-    for(int x = 0; x < 3; x++) // padding for each byte of the image
-    {
-      for(int w = 0; w < width; w++)
-      {
-        padding[i + x] = (unsigned char)(0x00);
-        padding[i * x] = (unsigned char)(image[i * x] + 1);
-      }
-    }
-  }
+	  for(int i = 0; i < paddedw; i++) // padded width for every 3 bytes of the image
+	  {
+	    for(int x = 0; x < 3; x++) // padding for each byte of the image
+	    {
+	      for(int w = 0; w < width; w++)
+	      {
+		padding[i + x] = (unsigned char)(0x00);
+		padding[i * x] = (unsigned char)(image[i * x] + 1);
+	      }
+	    }
+	  }
 
 	//int size_ = size * sizeof(char*);
 	for(int i = 0; i < size; i++) image[i] = 0x00;
